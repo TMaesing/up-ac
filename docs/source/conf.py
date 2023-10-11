4553562b8a5876e360662ad36fc8b378b3212ddf
DimitriWeiss <dimitri.weiss@uni-bielefeld.de>
2023-10-11 20:25:25 +0200

    added python_docs_theme to read the docs conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,8 +44,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "python_docs_theme"
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+# html_static_path = ['_static']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
